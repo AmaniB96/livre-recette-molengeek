767e35a3e6475119130754cbf13e7d923b92d271
amani <bataganiramani@gmail.com>
2025-03-13 15:51:24 +0100

    added card remove

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-let cards= document.querySelectorAll(".card")
-let images= document.querySelectorAll("img")
+let cards = document.querySelectorAll(".card")
+let images = document.querySelectorAll(".card-img-top")
+let footer = document.querySelector("footer")
 
-cards.forEach(card => {
-    card.addEventListener("mouseleave", ()=> {
-        card.style.backgroundColor= "";
+cards.forEach((card, index) => {
+    card.addEventListener("mouseleave", () => {
+        card.style.backgroundColor = "";
+    })
+    images[index].addEventListener("dblclick", () => {
+        card.remove()
     })
 });
 
@@ -16,23 +20,5 @@ cards.forEach(card => {
 cards.forEach(card => {
     card.addEventListener("click", ()=>{
         card.style.boxShadow = "10px 5px 5px cyan";
-    })
-});
-
-cards.forEach(card => {
-    card.addEventListener("dblclick", ()=>{
-        card.remove()
-    })
-});
-
-images.forEach(image => {
-    image.addEventListener("click", () =>{
-        let btn= document.createElement("button");
-        btn.style.backgroundColor ="red";
-        btn.innerText="X";
-        image.appendChild(btn)
-        btn.addEventListener("click", ()=>{
-            card.remove()
-        })
     })
 });
