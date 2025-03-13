c30067700f26935b586ecc5de37b7c22b43a3541
amani <bataganiramani@gmail.com>
2025-03-13 16:27:45 +0100

    test from submittion

--- js/main.js
+++ js/main.js
@@ -22,3 +22,31 @@ cards.forEach(card => {
         card.style.boxShadow = "10px 5px 5px cyan";
     })
 });
+
+
+let form=document.createElement("form")
+form.style.backgroundColor = "white"
+form.style.color= "grey"
+form.style.width= "175px"
+form.style.height= "35px"
+footer.appendChild(form)
+
+let input = document.createElement("input")
+input.type = "text"
+input.placeholder = "Type here..."
+input.style.width = "175px"
+input.style.height = "35px"
+form.appendChild(input)
+
+let btn=document.createElement("button")
+btn.innerText = "Newsletter"
+btn.style.backgroundColor = "rgba(0, 255, 200, 0.152)"
+btn.style.color= "white"
+btn.style.width= "100px"
+btn.style.height= "35px"
+btn.type ="submit"
+form.appendChild(btn) 
+
+btn.addEventListener("click", ()=>{
+    alert("L'adresse: "+ input + "est inscrit à la newsletter")
+})
